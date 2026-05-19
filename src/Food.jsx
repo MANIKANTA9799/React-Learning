64c9c55114508b16f29c404cfd9df617cfe49ccc
Manikanta <mytab123yy@gmail.com>
2026-05-19 13:10:19 +0530

    first commit

---
+++ src/Food.jsx
@@ -0,0 +1,18 @@
+function Food() {
+    const food1 = "orange ";
+    const food2 = "apple";
+    return (
+        <ul>
+            <li>
+                Available
+            </li>
+            <li>
+                {food1}
+            </li>
+            <li>
+                {food2}
+            </li>
+        </ul>
+    );
+}
+export default Food
---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return (
+        <footer>
+            <p>&copy;{new Date().getFullYear()} MANIKANTA</p>
+        </footer>
+    );
+}
+export default Footer
